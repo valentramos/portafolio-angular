325e333123db46d37040c775b0b33e70440591de
valentramos <ignacio1994.ir@gmail.com>
2018-09-12 01:15:02 -0400

    Base de datos en FireBase para la parte de About

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
-  constructor( private http:HttpClient ) { 
-    console.log('Servicio de infoPagina Listo!');
+  constructor( private http:HttpClient ) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
       });
    }
+
+   private cargarEquipo() {
+    this.http.get('https://valent-angular.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      console.log(resp);
+    });
+   }
+
 }
